efdc86b973f76820dee2a0989ed22a477d728473
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-02-16 00:06:04 -0800

    PHP Includes - Gwenever Pacifico

---
+++ php_includes/main.php
@@ -0,0 +1,8 @@
+<main id="main_content">
+    <div>
+    Main Content is here. I wish a double rainbow could be here instead. Why? Because double rainbows mean double
+    leprechauns, and double leprechauns mean double pots of gold. Who doesn't want twice as much gold? Think of all
+    the chocolate ponies you could buy with that gold. That's right, think about it. Chocolate. Ponies.
+    </div>
+    <img src="http://www.chocolatefantasies.com/horse-pony-Lolly.jpg">
+</main>
