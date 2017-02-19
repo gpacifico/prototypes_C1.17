87f3c4778e5f460751b2de6a0966925b4b0461ac
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-02-19 12:32:28 -0800

    version .1

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -1,1 +1,13 @@
+var chocolate_pony_http = require('http');
+
+var chocolate_pony_server = chocolate_pony_http.createServer(handleRequest);
+
+function handleRequest(request, response) {
+    console.log('Were ponies hit?', request.url);
+    response.end('Victory! Ponies hit!' + request.url);
+}
+
+chocolate_pony_server.listen(8888, function() {
+    console.log('Server Listening');
+});
 
