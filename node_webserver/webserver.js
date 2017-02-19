e0bf8d3f3da5008395d6af0f15536f7b12ca9211
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-02-19 14:33:08 -0800

    version .7

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -5,10 +5,31 @@ var chocolate_pony_server = chocolate_pony_http.createServer(handleRequest);
 
 function handleRequest(request, response) {
     console.log('Were ponies hit?', request.url);
-    chocolate_pony_file_system.readFile('./document_root/index.html', function (err, data) {
-        if (err) throw err;
-        response.end(data);
-        console.log(data);
+    var chocolate_pony_url = request.url;
+    if (chocolate_pony_url === '/') {
+        chocolate_pony_url = '/index.html';
+    }
+    chocolate_pony_file_system.readFile('./document_root' + chocolate_pony_url, function (err, data) {
+        if (err) {
+            chocolate_pony_url = '/404.html';
+            chocolate_pony_file_system.readFile('./document_root' + chocolate_pony_url, function (err, data) {
+                response.writeHead(404, {
+                    'Content-Type': 'text/html',
+                    'Gwen_likes': 'chocolate'
+                });
+                response.end(data);
+            })
+        }
+        else {
+            response.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Gwen_likes': 'ponies'
+            });
+            response.end(data);
+        }
+        // response.end(data);
+        // console.log(data);
+
     });
 
     //console.log('Were ponies hit?', request.url);
