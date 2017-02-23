c60abed6616ce1815a4f79a1a1b70d72855c7641
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-02-22 23:38:22 -0800

    MySQL Basics - Gwenever Pacifico

--- mysql_basics/mysql_queries.sql
+++ mysql_basics/mysql_queries.sql
@@ -1,1 +1,12 @@
 Place your queries below
+
+SELECT * FROM `students` WHERE `username` = 'miles'
+UPDATE `students` SET `email` = 'milesmakesprettywebsitesyay@gmail.com' WHERE `username` = 'milesmakespretty'
+INSERT INTO `students` SET `id` = 11, `name` = 'Jason', `email` = 'jasonisamarker@gmail.com', `age` = 25, `SSN` = 978645123, `username` = 'jasonisamarker', `password` = sha1('jasonisamrkerpassword'), `joined` = NOW(), `status` = 'active'
+DELETE FROM `students` WHERE `username` = 'jasonisamarker'
+
+SELECT * FROM `todo_items` WHERE `user_id` = 7
+INSERT INTO `todo_items` SET `title` = 'car', `details` = 'wash car', `timestamp` = now(), `user_id` = 7
+DELETE FROM `todo_items` WHERE `user_id` = 7 AND `title` = 'vacuum'
+UPDATE `todo_items` SET `details` = 'wax Honda' WHERE `user_id` = 7
+SELECT * FROM `students` WHERE `id` = 7
