c5867801c6490cde7db6980a5f2dd76fac444290
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-03-06 19:43:08 -0800

    PHP User Auth - Gwenever Pacifico

--- php_user_auth_basic/login_form.php
+++ php_user_auth_basic/login_form.php
@@ -1,1 +1,34 @@
 <!--login_form.php-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>User Auth</title>
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+        <script>
+            function to_login(){
+                $.ajax({
+                    url: 'login_handler.php',
+                    data: {"username": $('#username').val(), "password": $('#password').val()},
+                    cache: "false",
+                    method: "post",
+                    dataType: "json",
+                    success: function(response){
+                        console.log(response);
+                    },
+                    error: function(error){
+                        console.log(error);
+                    }
+                })
+            }
+        </script>
+    </head>
+    <body>
+        <form>
+            <input id="username" type="text" name="username" placeholder="Enter Username">
+            <br>
+            <input id="password" type="text" name="password" placeholder="Enter Password">
+            <br>
+            <button type="button" onclick="to_login()">Log In</button>
+        </form>
+    </body>
+</html>
--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -1,1 +1,43 @@
-<!--login_handler.php-->
+<?php
+
+//$encrypted_pass = sha1('cheesepassword');
+//print($encrypted_pass);
+//$encrypted_pass = sha1('sloganspassword');
+//print($encrypted_pass);
+//$encrypted_pass = sha1('timpassword');
+//print($encrypted_pass);
+//$encrypted_pass = sha1('kittenpassword');
+//print($encrypted_pass);
+//$encrypted_pass = sha1('cakepassword');
+//print($encrypted_pass);
+
+$user_info = [
+    ['id'=> 1, 'username'=>'cheeseislife', 'password' => 'f755833fc94668c23b007d91f5470e18d4177994'],
+    ['id'=> 2, 'username'=>'mileslikesslogans', 'password' => '151d0f1bacdd04156000edee834d1aa158836489'],
+    ['id'=> 3, 'username'=>'timgetsittogether', 'password' => 'ad73039a9617aa2bcac7de6a71f657b7d152744b'],
+    ['id'=> 4, 'username'=>'kittensdocombat', 'password' => 'c286fb7a50e0c52a445c6fc4126de64e3a103931'],
+    ['id'=> 5, 'username'=>'cakeisnotalie', 'password' => '048ec0611e699bec2635ec06fa28e08f733ca6e0']
+];
+
+$username = $_POST['username'];
+$password = sha1($_POST['password']);
+
+$output = [
+    'success'=>['success'=>true, 'user_id'=>'null', 'message'=>'You have logged in successfully as ' . $username],
+    'error'=>['success'=>false, 'message'=>'You are typing in the wrong thing']
+];
+
+for ($i = 0; $i < sizeof($user_info); $i++){
+    if ($username === $user_info[$i]['username']){
+        if ($password === $user_info[$i]['password']){
+            $_SESSION['user_id'] = $user_info[$i]['id'];
+            print_r(json_encode($output['success']));
+            return;
+        }
+    }
+}
+
+$output_string = json_encode($output['error']);
+print($output_string);
+
+?>
